d68279545b175d9923296d681f031caffd1327d8
Pranav <prnvbirajdar@gmail.com>
2022-04-15 15:34:07 -0400

    http body parsing

--- be/server.js
+++ be/server.js
@@ -3,6 +3,13 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 8080;
 const app = express();
 
+// We use these methods to parse the HTTP body
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: false
+}))
+
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 app.use('/api/goals', require('./routes/goalRoutes'))
