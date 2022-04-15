1c2267442a60b5879191d3fbf04b441f949df412
Pranav <prnvbirajdar@gmail.com>
2022-04-15 11:24:30 -0400

    First route

--- be/server.js
+++ be/server.js
@@ -1,1 +1,15 @@
-console.log('hiddd');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.get('/api/goals', (req, res) => {
+	res.json(
+		{
+				message: 'Hello World!'
+		}
+	);
+})
